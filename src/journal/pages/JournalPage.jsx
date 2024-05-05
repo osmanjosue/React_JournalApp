41bbb123f880c58eddab5f112e6734b2ff642bcd
osmanjosue <osmanjosue@gmail.com>
2024-05-04 19:14:29 -0600

    Seccion 17 => Clase: Configuracion de MUI con Vite => theme+font

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,12 @@
+// import { MailOutline } from '@mui/icons-material';
+import { Typography } from '@mui/material';
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
-  )
-}
+    // <Typography component='h1'>JournalPage</Typography>
+    <>
+      <Typography variant="h1">JournalPage</Typography>
+      {/* <MailOutline /> */}
+    </>
+  );
+};
