850838965bb981c0f86df70f50ca76e0a467f2fa
osmanjosue <osmanjosue@gmail.com>
2024-05-11 14:24:15 -0600

    Seccion 17 => Clase: NavBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material';
+import { NavBar } from '../components';
 
 export const JournalLayout = ({ children }) => {
   const drawerWidth = 240;
 
   return (
     <Box sx={{ display: 'flex' }}>
-      {/* Navbar */}
+      <NavBar drawerWidth={drawerWidth} />
       {/* Sidebar drawerWidth*/}
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
