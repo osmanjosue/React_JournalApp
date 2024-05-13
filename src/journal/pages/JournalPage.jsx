14b684788ec014e2e6ed5003f29081ae4e30007d
osmanjosue <osmanjosue@gmail.com>
2024-05-13 15:41:38 -0600

    Seccion 17 => Clase: NothingSelectedView-No hay nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,17 @@
 // import { MailOutline } from '@mui/icons-material';
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quis
         tenetur assumenda adipisci, impedit aliquam voluptate fugiat. Deleniti
         suscipit eaque, labore doloremque quis laboriosam, distinctio excepturi
         nisi officia mollitia dolores.
-      </Typography>
-      {/* NothingSelected */}
+      </Typography> */}
+      <NothingSelectedView />
       {/* NoteView */}
     </JournalLayout>
   );
