07820a409e9c57197a99ab61d18ecb90571e7010
osmanjosue <osmanjosue@gmail.com>
2024-05-15 22:12:42 -0600

    Seccion 17 => Clase: Boton Flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,8 @@
 // import { MailOutline } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
   return (
@@ -12,7 +14,21 @@ export const JournalPage = () => {
         nisi officia mollitia dolores.
       </Typography> */}
       {/* <NothingSelectedView /> */}
-      <NoteView/>
+      <NoteView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', Opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}>
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+      
     </JournalLayout>
   );
 };
