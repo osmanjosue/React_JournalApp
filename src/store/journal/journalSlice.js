9f1f4ac4b91b1dedc115a21f3205b3026a966049
osmanjosue <osmanjosue@gmail.com>
2024-07-15 21:21:18 -0600

    Seccion 19 => Clase: JournalSlice

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,51 @@
+import {
+    createSlice
+} from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSaving: true,
+        messageSaved: '',
+        notes: [],
+        active: null,
+        //acrive: {
+        //id: 'ABC123',
+        //title: '',
+        //body: '',
+        //date: 1234567,
+        //imageUrls: [], //https://foto1.jpg, https://foto2.jpg,, https://foto3.jpg
+    },
+    reducers: {
+        addNewEmptyNote: (state, action) => {
+
+        },
+        setActiveNote: (state, action) => {
+
+        },
+        setNotes: (state, action) => {
+
+        },
+        setSaving: (sate) => {
+
+        },
+        updateNote: (state, action) => {
+
+        },
+        deleteNoteById: (state, action) => {
+
+        },
+
+    }
+});
+
+
+// Action creators are generated for each case reducer function
+export const {
+    addNewEmptyNote,
+    setActiveNote,
+    setNotes,
+    setSaving,
+    updateNote,
+    deleteNoteById,
+} = journalSlice.actions;
