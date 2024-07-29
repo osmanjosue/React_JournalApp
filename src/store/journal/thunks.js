f0f2f4dd2ba76c2c4a1a3984e7fad594e2130bf8
osmanjosue <osmanjosue@gmail.com>
2024-07-28 21:41:23 -0600

    Seccion 20 => Clase: Cargar notas del Firestore

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,11 +1,12 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote } from "./";
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./";
+import { loadNotes } from "../../helpers";
 
 export const startNewNote = () => {
     return async (dispatch, getState) => {
 
-        dispatch( savingNewNote());
+        dispatch(savingNewNote());
 
         const { uid } = getState().auth;
 
@@ -22,5 +23,15 @@ export const startNewNote = () => {
         dispatch(addNewEmptyNote(newNote));
         dispatch(setActiveNote(newNote));
 
+    }
+}
+
+export const startLoadingNotes = () => {
+    return async (dispatch, getState) => {
+        const { uid } = getState().auth;
+        if (!uid) throw new Error('El uid del usuario no existe');
+
+        const notes = await loadNotes(uid);
+        dispatch(setNotes(notes));
     }
 }
